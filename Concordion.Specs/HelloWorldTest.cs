9cd42487d5607dfbda7c09bb7400e08fc5da62ad
Thibaut Géry <tgery@octo.com>
2014-12-06 16:19:55 +0100

    add concordion sample project

---
+++ Concordion.Specs/HelloWorldTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Concordion.Integration;
+
+namespace Concordion.Specs
+{
+    [ConcordionTest]
+    public class HelloWorldTest
+    {
+
+        public string GetGreeting()
+        {
+            return "Hello World!";
+        }
+    }
+}
