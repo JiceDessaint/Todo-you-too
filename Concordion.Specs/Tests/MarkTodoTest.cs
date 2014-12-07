04f92a101c031615c020a4e04904004fbe8cb4a2
Thibaut Géry <tgery@octo.com>
2014-12-07 20:20:25 +0100

    add mark task as done test in concordion

---
+++ Concordion.Specs/Tests/MarkTodoTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concordion.Integration;
+using TodoYouToo.Entities;
+using Concordion.Specs.Utilities;
+using Caliburn.Micro;
+using TodoYouToo;
+using TodoYouToo.Data;
+
+namespace Concordion.Specs.Tests
+{
+    [ConcordionTest]
+    class MarkTodoTest : Fixture
+    {
+        public void MarkTaskNamed(String name) {
+
+                var item = mainVM.TodoItems.Where(t => t.Text == name).SingleOrDefault();
+                if (item == null)
+                    throw new Exception("can't find task");
+                item.IsDone = true;
+                mainVM.SaveTodo(item);
+
+        }
+      
+
+    }
+}
